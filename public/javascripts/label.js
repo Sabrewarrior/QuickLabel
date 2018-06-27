8f39615104ff5c421d13db2408753359a2099647
Sabrewarrior <Sabre8463>
2018-06-27 19:21:11 -0400

    - Improved search functionality

--- public/javascripts/label.js
+++ public/javascripts/label.js
@@ -1,57 +1,46 @@
 $(document).ready(function () {
-    var height = $("#one").height();
+    let height = $("#one").height();
     if(height > 0) {
-        $("#two").css("margin-top",height);
+        $( '#two' ).css("margin-top", height);
     }
-    function saveID(label){
-        $('#save')[0].textContent = 'Save';
-        let cur_id = document.getElementById("cur_id").textContent;
-        let node_list = document.getElementsByName(label[0].attributes.name.value);
-        let values_list = [];
-        node_list.forEach(function(item){
-            if (item.checked || item.type === "text" || item.type === "date"){
-                values_list.push(item.value)
-            }
-        });
-        $.ajax("save/" + cur_id, {
-            type: 'POST',
-            data: JSON.stringify({"variable": label[0].attributes.name.value,
-                "values": values_list}),
-            contentType: 'application/json'
-        });
-    }
+    let cur_id = document.getElementById("cur_id").textContent;
+    let searchDisplay = document.getElementById("searchbar");
+    let searchText = document.getElementById("searchText");
+
     // the input field
-    var $input = $("input[id='searchText']"),
+    let $searchInput = $("input[id='searchText']"),
         // clear button
-        $clearBtn = $("button[data-search='clear']"),
+        $searchClearBtn = $("button[data-search='clear']"),
         // prev button
-        $prevBtn = $("button[data-search='prev']"),
+        $searchPrevBtn = $("button[data-search='prev']"),
         // next button
-        $nextBtn = $("button[data-search='next']"),
+        $searchNextBtn = $("button[data-search='next']"),
         // the context where to search
-        $content = $(".searchable"),
+        $searchContent = $(".searchable"),
         // jQuery object to save <mark> elements
-        $results,
+        $searchResults,
         // the class that will be appended to the current
         // focused element
         currentClass = "current",
         // top offset for the jump (the search bar)
         offsetTop = height + 100,
         // the current index of the focused element
-        currentIndex = 0;
+        searchCurrentIndex = 0;
 
     /**
-     * Jumps to the element matching the currentIndex
+     * Jumps to the element matching the searchCurrentIndex
      */
-    function jumpTo() {
-        if ($results.length) {
-            var position,
-                $current = $results.eq(currentIndex);
-            $results.removeClass(currentClass);
+    function jumpTo(performJump) {
+        if ($searchResults.length) {
+            let position,
+                $current = $searchResults.eq(searchCurrentIndex);
+            $searchResults.removeClass(currentClass);
             if ($current.length) {
                 $current.addClass(currentClass);
-                position = $current.offset().top - offsetTop;
-                window.scrollTo(0, position);
+                if(performJump){
+                    position = $current.offset().top - offsetTop;
+                    window.scrollTo(0, position);
+                }
             }
         }
     }
@@ -60,124 +49,138 @@ $(document).ready(function () {
      * Searches for the entered keyword in the
      * specified context on input
      */
-    $input.on("input", function() {
-        var searchVal = this.value;
-        $content.unmark({
-            done: function() {
-                $content.mark(searchVal, {
-                    separateWordSearch: true,
-                    element: "span",
-                    className: "mark-highlight",
-                    done: function() {
-                        $results = $content.find(".mark-highlight");
-                        currentIndex = 0;
-                        jumpTo();
-                    }
-                });
-            }
-        });
+    function getSearch(jump){
+        if ($searchInput.prop("value") !== undefined || $searchInput.prop("value") !== ""){
+            $searchContent.unmark({
+                done: function() {
+                    $searchContent.mark($searchInput.prop("value"), {
+                        separateWordSearch: false,
+                        element: "span",
+                        className: "mark-highlight",
+                        done: function() {
+                            $searchResults = $searchContent.find(".mark-highlight");
+                            if (jump){
+                                searchCurrentIndex = 0;
+                            }
+                            jumpTo(jump);
+                        },
+                        noMatch: function(){
+                        }
+                    });
+                }});
+        } else {
+            searchCurrentIndex = 0;
+        }
+    }
+
+    $searchInput.on("input", function(){
+        getSearch(true);
     });
 
     /**
      * Clears the search
      */
-    $clearBtn.on("click", function() {
-        $content.unmark();
-        $input.val("").focus();
-        document.getElementById("searchbar").style.display = "none";
-
+    function clearSearch(){
+        $searchContent.unmark();
+        searchDisplay.style.display = "none";
         return false;
-    });
+    }
+
+    $searchClearBtn.on("click", clearSearch);
 
     /**
      * Next and previous search jump to
      */
-    $nextBtn.add($prevBtn).on("click", function() {
-        if ($results !== undefined && $results.length) {
-            currentIndex += $(this).is($prevBtn) ? -1 : 1;
-            if (currentIndex < 0) {
-                currentIndex = $results.length - 1;
+    $searchNextBtn.add($searchPrevBtn).on("click", function() {
+        if ($searchResults !== undefined && $searchResults.length) {
+            searchCurrentIndex += $(this).is($searchPrevBtn) ? -1 : 1;
+            if (searchCurrentIndex < 0) {
+                searchCurrentIndex = $searchResults.length - 1;
             }
-            if (currentIndex > $results.length - 1) {
-                currentIndex = 0;
+            if (searchCurrentIndex > $searchResults.length - 1) {
+                searchCurrentIndex = 0;
             }
 
-            jumpTo();
+            jumpTo(true);
         }
         return false;
     });
 
+    function saveID(variableName){
+        $( '#save' ).prop("textContent", "Save");
+        let values_list = [];
+        // Use ID for faster traversing
+        let node_list = document.getElementById(variableName).getElementsByClassName("single_element");
+
+        [].forEach.call(node_list, function(item){
+            if (item.checked || item.type === "text" || item.type === "date"){
+                values_list.push(item.value)
+            }
+        });
+        $.ajax("save/" + cur_id, {
+            type: 'POST',
+            data: JSON.stringify({"variable": variableName,
+                "values": values_list}),
+            contentType: 'application/json'
+        });
+    }
+
     $( '.dropdown-menu a' ).on( 'click', function( event ) {
-        var $target = $( event.currentTarget ),
-            val = $target.attr( 'data-value' ),
-            $inp = $target.find( 'input' ),
-            idx;
-        // console.log($inp.prop("checked") === false);
+        let $target = $( event.currentTarget ),
+            $inp = $target.find( '.single_element' );
         $inp.prop("checked", !$inp.prop("checked"));
         $( event.target ).blur();
-        saveID($inp);
+        saveID($target.prop("name"));
         return false;
     });
 
-    $(".input_form").change(function(){
-        // console.log("HERE");
-        let label = $( this );
-        // console.log(label);
-        // console.log(label[0].attributes.name.value);
-
+    $( '.input_form' ).change(function(){
+        let label = $( this ).prop("name");
         saveID(label);
     });
 
-    $( '.dropdown-menu input' ).on( 'click', function( event ) {
-        var $target = $( event.currentTarget );
-        // console.log($target);
-        $target.prop("checked", !$target.prop("checked"));
-        $( event.target ).blur();
-
-        return false;
-    });
-
-    $('#save').click(function(){
-        $('#save')[0].textContent = 'Saving';
+    $( '#save' ).click(function(){
+        $( '#save' ).prop("textContent", "Saving");
         $.ajax("/save", {
             type: 'GET',
             success: function(data) {
-                $('#save')[0].textContent = 'Saved';
+                $( '#save' ).prop("textContent", "Saved");
             },
             error: function(data) {
-                $('#save')[0].textContent = 'Failed';
+                $( '#save' ).prop("textContent", "Failed");
                 alert(JSON.stringify(data.responseJSON).slice(1,-1).split(",").join("\n"))
             }
         })
     });
 
     window.addEventListener('keydown', function(event){
-        var searchText = document.getElementById("searchText");
         if (event.defaultPrevented) {
             return; // Should do nothing if the default action has been cancelled
         }
-        var handle_find = false,
+        let handle_find = false,
             handle_escape = false;
         if (event.key !== undefined){
-            // console.log(event.key);
             if (event.key === "F3" || (event.ctrlKey && event.key === "f")){
                 handle_find = true;
-            }
-            if (event.key === "Escape" && document.activeElement === searchText) {
+            } else if (event.key === "Escape" && document.activeElement === searchText) {
                 handle_escape = true;
             }
-        } else if (event.keyIdentifier !== undefined){
-            console.log(event.keyIdentifier);
-        } else if (event.keyCode === 114 || (event.ctrlKey && event.keyCode === 70)) {
-            handled = true;
+        } else if (event.keyCode !== undefined){
+            if (event.keyCode === 114 || (event.ctrlKey && event.keyCode === 70)){
+                handle_find = true;
+            } else if (event.keyCode === 27 && document.activeElement === searchText) {
+                handle_escape = true;
+            }
         }
+
         if (handle_escape) {
-            document.getElementById("searchbar").style.display = "none";
+            clearSearch();
             // Suppress "double action" if event handled
             event.preventDefault();
         } else if (handle_find) {
-            document.getElementById("searchbar").style.display = "block";
+            searchDisplay.style.display = "block";
             searchText.focus();
+            getSearch(false);
             // Suppress "double action" if event handled
             event.preventDefault();
         }
@@ -188,6 +191,4 @@ $(document).ready(function () {
             event.preventDefault();
         }
     });
-
-
 });
